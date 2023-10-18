26f454c03b0da3584119433f079346d5f49c2bf7
Khan-HB <hafsah999@gmail.com>
2023-10-17 23:19:45 -0400

    created hooks in the user model, beforeCreate and beforeUpdate, Connected the model to the Sequelize instance and specified options , exported the model

--- models/User.js
+++ models/User.js
@@ -106,3 +106,26 @@ User.init(
     },
   },
   {
+    // Hooks that automatically hash the user's password before creation and update
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10); // Hash the password before creating a new user
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10); // Hash the password before updating a user
+        return updatedUserData;
+      },
+    },
+    // Connect the model to the Sequelize instance and specify options
+    sequelize, // Pass the Sequelize instance
+    timestamps: false, // Disable timestamps (createdAt and updatedAt)
+    freezeTableName: true, // Prevent Sequelize from pluralizing the table name
+    underscored: true, // Use underscored naming for fields (e.g., created_at)
+    modelName: 'user', // Name of the model
+  }
+);
+
+// Export the User model to use in other parts of the application
+module.exports = User;
+
