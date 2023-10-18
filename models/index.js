453e4c57d9aee3cde53e99f03c42f6b228cc6fe5
Khan-HB <hafsah999@gmail.com>
2023-10-18 00:29:58 -0400

    added the post assocaition to the models/index file, post to user and post to comment

--- models/index.js
+++ models/index.js
@@ -26,3 +26,18 @@ Comment.belongsTo(User, {
 Comment.belongsTo(Post, {
     foreignKey: 'post_id', // Create a foreign key 'post_id' in the Comment model to reference Posts
 });
+
+// A Post belongs to a User (many-to-one relationship).
+Post.belongsTo(User, {
+    foreignKey: 'user_id', // Create a foreign key 'user_id' in the Post model to reference Users
+    onDelete: 'CASCADE', // When a User is deleted, also delete their associated Posts
+});
+
+// A Post can have multiple Comments (one-to-many relationship).
+Post.hasMany(Comment, {
+    foreignKey: 'post_id', // Create a foreign key 'post_id' in the Comment model to associate Comments with Posts
+    onDelete: 'CASCADE', // When a Post is deleted, also delete its associated Comments
+});
+
+// Export the User, Comment, and Post models and their associations
+module.exports = { User, Comment, Post };
