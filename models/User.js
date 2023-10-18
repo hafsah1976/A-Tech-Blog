6cece42fcac20a62c7c2e8b3bd59f687c5338e82
Khan-HB <hafsah999@gmail.com>
2023-10-17 23:53:32 -0400

    finished the User model to the best of my knowledge, may add dayjs later.

--- models/User.js
+++ models/User.js
@@ -9,7 +9,6 @@ class User extends Model {
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password); // Compare the provided password with the stored hashed password
   }
-<<<<<<< HEAD
 }
 
 // Initialize the User model with the specified attributes and configuration
@@ -68,64 +67,3 @@ User.init(
 
 // Export the User model
 module.exports = User;
-=======
-}
->>>>>>> 9dfa614 (coding the user model, made necessary imports, including bcrypt, made the user class, and instance method called checkPassowrd for validating epassword)
-
-// Initialize the User model with the specified attributes and configuration
-User.init(
-  {
-    // Define the 'id' field
-    id: {
-      type: DataTypes.INTEGER, // Data type for the 'id' field (integer)
-      allowNull: false, // 'id' cannot be null
-      primaryKey: true, // 'id' is the primary key
-      autoIncrement: true, // 'id' increments automatically
-    },
-    // Define the 'name' field
-    name: {
-      type: DataTypes.STRING, // Data type for the 'name' field (string)
-      allowNull: false, // 'name' cannot be null
-    },
-    // Define the 'email' field
-    email: {
-      type: DataTypes.STRING, // Data type for the 'email' field (string)
-      allowNull: false, // 'email' cannot be null
-      unique: true, // 'email' values must be unique
-      validate: {
-        isEmail: true, // Use Sequelize validation to ensure it's a valid email
-      },
-    },
-    // Define the 'password' field
-    password: {
-      type: DataTypes.STRING, // Data type for the 'password' field (string)
-      allowNull: false, // 'password' cannot be null
-      validate: {
-        len: [8], // Use Sequelize validation to require a minimum password length of 8 characters
-      },
-    },
-  },
-  {
-    // Hooks that automatically hash the user's password before creation and update
-    hooks: {
-      beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10); // Hash the password before creating a new user
-        return newUserData;
-      },
-      beforeUpdate: async (updatedUserData) => {
-        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10); // Hash the password before updating a user
-        return updatedUserData;
-      },
-    },
-    // Connect the model to the Sequelize instance and specify options
-    sequelize, // Pass the Sequelize instance
-    timestamps: false, // Disable timestamps (createdAt and updatedAt)
-    freezeTableName: true, // Prevent Sequelize from pluralizing the table name
-    underscored: true, // Use underscored naming for fields (e.g., created_at)
-    modelName: 'user', // Name of the model
-  }
-);
-
-// Export the User model to use in other parts of the application
-module.exports = User;
-
