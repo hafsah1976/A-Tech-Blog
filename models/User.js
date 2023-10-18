b7be6493c523fb6613dd274938f9a946de98809e
Khan-HB <hafsah999@gmail.com>
2023-10-17 23:17:23 -0400

    added columns and attribute to the user model in the init method

--- models/User.js
+++ models/User.js
@@ -10,3 +10,37 @@ class User extends Model {
     return bcrypt.compareSync(loginPw, this.password); // Compare the provided password with the stored hashed password
   }
 }
+// Initialize the User model with the specified attributes and configuration
+User.init(
+  {
+    // Define the 'id' field
+    id: {
+      type: DataTypes.INTEGER, // Data type for the 'id' field (integer)
+      allowNull: false, // 'id' cannot be null
+      primaryKey: true, // 'id' is the primary key
+      autoIncrement: true, // 'id' increments automatically
+    },
+    // Define the 'name' field
+    name: {
+      type: DataTypes.STRING, // Data type for the 'name' field (string)
+      allowNull: false, // 'name' cannot be null
+    },
+    // Define the 'email' field
+    email: {
+      type: DataTypes.STRING, // Data type for the 'email' field (string)
+      allowNull: false, // 'email' cannot be null
+      unique: true, // 'email' values must be unique
+      validate: {
+        isEmail: true, // Use Sequelize validation to ensure it's a valid email
+      },
+    },
+    // Define the 'password' field
+    password: {
+      type: DataTypes.STRING, // Data type for the 'password' field (string)
+      allowNull: false, // 'password' cannot be null
+      validate: {
+        len: [8], // Use Sequelize validation to require a minimum password length of 8 characters
+      },
+    },
+  },
+  {
