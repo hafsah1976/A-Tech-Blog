58fde063e280872ee9ac1b86c20f2eed762fa0ae
Khan-HB <hafsah999@gmail.com>
2023-10-17 23:56:29 -0400

    adding to Comment.js Model, added the dayjs import here to check if I can have my ccustom date format in the column of  date that refers to date a comment was  posted

--- models/Comment.js
+++ models/Comment.js
@@ -1,1 +1,16 @@
 
+// Import necessary modules and dependencies
+const { Model, DataTypes } = require('sequelize'); // Import required Sequelize classes and modules
+const sequelize = require('../config/config'); // Import the Sequelize instance from a configuration file
+const dayjs = require('dayjs'); // Import the dayjs library for date formatting
+
+class Comment extends Model {}
+Comment.init(
+    {
+        // Define the comment 'id' field
+        id: {
+            type: DataTypes.INTEGER, // Data type for the 'id' field (integer)
+            allowNull: false, // 'id' cannot be null
+            primaryKey: true, // 'id' is the primary key
+            autoIncrement: true, // 'id' increments automatically
+        },
